7ff617392a63849099a01fa55ae580947a46f616
MelonMars <carterpfaff@gmail.cpm>
2021-03-25 20:51:52 -0400

    Update dankmemerautotyper.py

--- dankmemerautotyper.py
+++ dankmemerautotyper.py
@@ -2,6 +2,7 @@ import pyautogui as pg
 import pyperclip as pc
 import time
 
+reps = int(input("How many reps?"))
 time.sleep(2)
 def main():
     pc.copy("pls beg")
@@ -9,5 +10,5 @@ def main():
     pg.press("enter")
     time.sleep(45)
 
-for i in range(3):
+for i in range(reps):
     main()
