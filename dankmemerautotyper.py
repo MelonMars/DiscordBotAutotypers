4c12b67808323e37693b7f6b7402e990a17bf67a
MelonMars <carterpfaff@gmail.cpm>
2021-03-25 20:44:21 -0400

    ability to autotype THREE TIMES :0

--- dankmemerautotyper.py
+++ dankmemerautotyper.py
@@ -1,3 +1,13 @@
-import pyautogui
+import pyautogui as pg
+import pyperclip as pc
 import time
-import numpy as np
+
+time.sleep(2)
+def main():
+    pc.copy("pls beg")
+    pg.hotkey("ctrl", "v")
+    pg.press("enter")
+    time.sleep(45)
+
+for i in range(3):
+    main()
